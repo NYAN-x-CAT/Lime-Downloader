da31242f6e4c46d029e43666230738e98022bfd8
NYAN-x-CAT <NYANxCAT@pm.me>
2018-07-26 12:25:49 +0300

    Initial commit

---
+++ Lime Downloader/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Lime Downloader")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("NYAN CAT")>
+<Assembly: AssemblyProduct("Lime Downloader")>
+<Assembly: AssemblyCopyright("Copyright © NYANCAT 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7db8f970-b1a3-46f7-b521-f01cbc4fb4cc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
